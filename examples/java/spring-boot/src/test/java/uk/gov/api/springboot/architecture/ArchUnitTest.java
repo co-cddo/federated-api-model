98bddccb05c9465477ff61c0fc668ace22304d81
Jamie Tanna <jamie@jamietanna.co.uk>
2022-01-24 10:04:40 +0000

    Add initial ArchUnit rules
    Arch Unit can help us codify our technical architecture, and we can
    start with adding two rules:
    
    - ensuring that we use `final` fields for immutable classes where
      possible
    - ensure we don't use field injection with Spring, because it's not
      recommended by a lot of people - including me
    
    This allows us to shift from requiring overhead in code review, to
    enforcing it automagically.

---
+++ examples/java/spring-boot/src/test/java/uk/gov/api/springboot/architecture/ArchUnitTest.java
@@ -0,0 +1,36 @@
+package uk.gov.api.springboot.architecture;
+
+import static com.tngtech.archunit.base.DescribedPredicate.describe;
+import static com.tngtech.archunit.base.DescribedPredicate.equalTo;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.elements.GivenClassesConjunction;
+import org.junit.jupiter.api.Nested;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@AnalyzeClasses(packages = "uk.gov.api.springboot")
+class ArchUnitTest {
+  @ArchTest
+  @SuppressWarnings("unused")
+  ArchRule requireFinalFields =
+      classesThatAreNotTests().and().haveNameNotMatching(".*Dao").should().haveOnlyFinalFields();
+
+  @ArchTest
+  @SuppressWarnings("unused")
+  ArchRule requireNoAutowiredFieldInjection =
+      classesThatAreNotTests()
+          .and()
+          .containAnyFieldsThat(
+              describe(
+                  "are Autowired by Spring",
+                  f -> f.tryGetAnnotationOfType(Autowired.class).isPresent()))
+          .should()
+          .containNumberOfElements(equalTo(0));
+
+  private GivenClassesConjunction classesThatAreNotTests() {
+    return classes().that().haveNameNotMatching(".*Test").and().areNotAnnotatedWith(Nested.class);
+  }
+}
