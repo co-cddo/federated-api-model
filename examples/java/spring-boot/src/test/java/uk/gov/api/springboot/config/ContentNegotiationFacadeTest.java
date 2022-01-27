2e37ccbcfd5ac89e8e0f8034218feb4cf2b4f409
Jamie Tanna <jamie@jamietanna.co.uk>
2022-01-27 15:14:44 +0000

    sq

---
+++ examples/java/spring-boot/src/test/java/uk/gov/api/springboot/config/ContentNegotiationFacadeTest.java
@@ -0,0 +1,12 @@
+package uk.gov.api.springboot.config;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ContentNegotiationFacadeTest {
+  @Test
+  void todo() {
+    fail("TODO");
+  }
+}
