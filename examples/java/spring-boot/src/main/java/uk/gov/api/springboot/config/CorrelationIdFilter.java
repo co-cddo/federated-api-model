58ebfb3bd432ffce4f3440c6545b15bdaf6c0eb6
Bethan Palmer <bethan.palmer@hotmail.com>
2022-01-31 16:47:40 +0000

    Add the correlation-id to the MDC
    We want to have the correlation-id in our logs to make it easier to track
    requests and debug issues.  We've added some tests to ensure the correlation-id
    is added to the MDC and that we're logging a message when the filter is called.

--- examples/java/spring-boot/src/main/java/uk/gov/api/springboot/config/CorrelationIdFilter.java
+++ examples/java/spring-boot/src/main/java/uk/gov/api/springboot/config/CorrelationIdFilter.java
@@ -8,26 +8,36 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import me.jvt.uuid.Patterns;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
 public class CorrelationIdFilter extends OncePerRequestFilter {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(CorrelationIdFilter.class);
   private static final String CORRELATION_ID_HEADER = "correlation-id";
 
   private final ErrorResponseDecorator errorResponseDecorator;
+  private final MdcFacade mdcFacade;
 
-  public CorrelationIdFilter(ErrorResponseDecorator errorResponseDecorator) {
+  public CorrelationIdFilter(ErrorResponseDecorator errorResponseDecorator, MdcFacade mdcFacade) {
     this.errorResponseDecorator = errorResponseDecorator;
+    this.mdcFacade = mdcFacade;
   }
 
   @Override
   protected void doFilterInternal(
-      HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      @NonNull HttpServletRequest request,
+      @NonNull HttpServletResponse response,
+      @NonNull FilterChain filterChain)
       throws ServletException, IOException {
     String correlationId = retrieveOrGenerate(request);
     if (!correlationId.matches(Patterns.UUID_V4_STRING)) {
+      LOGGER.warn(
+          "The value provided in the `correlation-id` header was not provided in a valid UUID format");
       errorResponseDecorator.decorateWithNegotiation(
           request,
           response,
@@ -35,9 +45,12 @@ public class CorrelationIdFilter extends OncePerRequestFilter {
       return;
     }
     try {
+      mdcFacade.put(MdcFacade.CORRELATION_ID, correlationId);
+      LOGGER.info("A request was sent with correlation-id {}", correlationId);
       filterChain.doFilter(request, response);
     } finally {
       response.addHeader(CORRELATION_ID_HEADER, correlationId);
+      mdcFacade.clear();
     }
   }
 
