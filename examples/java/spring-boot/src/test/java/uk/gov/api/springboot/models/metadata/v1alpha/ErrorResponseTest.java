1a044970c7d082e5b0d4e089ad6c5bebbc956322
Bethan Palmer <bethan.palmer@hotmail.com>
2022-01-31 13:41:46 +0000

    Add the schema for the error response

---
+++ examples/java/spring-boot/src/test/java/uk/gov/api/springboot/models/metadata/v1alpha/ErrorResponseTest.java
@@ -0,0 +1,46 @@
+package uk.gov.api.springboot.models.metadata.v1alpha;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+import uk.gov.api.models.metadata.v1alpha.ErrorResponse;
+
+@JsonTest
+public class ErrorResponseTest {
+
+  @Autowired private JacksonTester<ErrorResponse> jacksonTester;
+
+  private JsonContent<ErrorResponse> jsonContent;
+
+  @Nested
+  class Serialization {
+
+    @Test
+    void errorIsSerialized() throws IOException {
+      var errorResponse = new ErrorResponse();
+      errorResponse.setError(ErrorResponse.Error.INVALID_REQUEST);
+
+      jsonContent = jacksonTester.write(errorResponse);
+
+      assertThat(jsonContent).extractingJsonPathStringValue("error").isEqualTo("invalid_request");
+    }
+
+    @Test
+    void errorDescriptionIsSerialized() throws IOException {
+      var errorResponse = new ErrorResponse();
+      errorResponse.setErrorDescription("An error");
+
+      jsonContent = jacksonTester.write(errorResponse);
+
+      assertThat(jsonContent)
+              .extractingJsonPathStringValue("error-description")
+              .isEqualTo("An error");
+    }
+  }
+}
